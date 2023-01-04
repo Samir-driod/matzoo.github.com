e4afcb7f7250126b9f634a8f34903444a353f5af
Samir-driod <73778825+Samir-driod@users.noreply.github.com>
2023-01-04 12:22:27 +0100

    Update set.js

--- www.matzoo.pl/set.js
+++ www.matzoo.pl/set.js
@@ -4,7 +4,7 @@ function myFunction() {
 var span_Text = document.getElementById("ok").innerHTML;
 let val = 5 + parseInt(span_Text);
 console.log(span_Text)
- document.getElementById("ok").innerHTML = val;
+ #document.getElementById("ok").innerHTML = val;
  
  document.getElementById("nok").innerHTML = 0;
 }
